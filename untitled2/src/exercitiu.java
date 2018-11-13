44db3f24f84f6d88542e5cf20adb3b5251b3a237
bajoe9 <44846502+bajoe9@users.noreply.github.com>
2018-11-13 12:09:09 +0200

    Add files via upload

---
+++ untitled2/src/exercitiu.java
@@ -0,0 +1,16 @@
+public class exercitiu {
+    public class helloworld{
+public  void main(String[] args){
+
+    System.out.println("hellow world");
+
+
+int age =10;
+if (age >7){
+
+
+
+
+
+}}}
+
---
+++ untitled2/src/exercitiu_age.java
@@ -0,0 +1,25 @@
+public class exercitiu_age { public class helloworld{
+    {
+
+
+        int age = 23;
+                if (age <6){
+                    System.out.println("You are Generation Alpth");
+                }
+               else if (age <21){
+                    System.out.println("You are Generation Z");
+    }
+    else if (age <36){
+        System.out.println("You are Generation Y");
+                }
+                else if (age <51){
+                    System.out.println("You are Generation X");
+                }
+                else if (age <70){
+                    System.out.println("You are a Baby Boomers");
+                }
+                else if (age >70){
+                    System.out.println("You are from the Builder Generation");
+                }
+
+}}}
